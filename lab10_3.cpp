4c9ee4f02fd781cf1f471498cd1da57edc1a5386
khaola737 <khaokla110150@gmail.com>
2025-12-26 13:55:26 +0700

    commit add lab10_3.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    ifstream fin("score.txt");
+
+    double x, sum = 0.0, sumsq = 0.0;
+    int n = 0;
+
+    while (fin >> x) {
+        sum += x;
+        sumsq += x * x;
+        n++;
+    }
+    fin.close();
+
+    double mean = sum / n;
+    double sd = sqrt((sumsq / n) - (mean * mean));
+
+    cout << "Number of data = " << n << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << sd ;
+
+    return 0;
+}
